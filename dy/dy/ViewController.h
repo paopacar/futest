9eb5df07c0377530015d2c8b8dc307ce9e05addf
fuwenqiang <fwqnl@sina.com>
2018-10-18 20:16:50 +0800

    hh
    创建文件

---
+++ dy/dy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dy
+//
+//  Created by wenqiang8 on 2018/10/18.
+//  Copyright © 2018年 fuwenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dy/dy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dy
+//
+//  Created by wenqiang8 on 2018/10/18.
+//  Copyright © 2018年 fuwenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
